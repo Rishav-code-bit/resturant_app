0b061fbcaf392ef901e0662ce05f1ded28bb525f
Rishav <rishavkumar99057@gmail.com>
2022-12-03 23:24:03 +0530

    Home-page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import background from "../components/images/home_bg.png";
+import Button from '@mui/material/Button'
 import "./home.css";
 const home = () => {
     return (
         <div classname="homebg" style={{ backgroundImage: `url(${background})`, height: "100vh",backgroundSize: "cover",backgroundAttachment: "fixed", backgroundRepeat: "no-repeat" }}>
-            <div className="homename">
-                Home
+            <div className='home-name'>
+              <h1>THE <br/> SUNDAY BITE</h1>
+            </div>
+            <div className='home-name-two'>
+              <h2>Delicious food for everybody</h2>
+              <Button variant="outlined">See More</Button>
             </div>
         </div>
     )
