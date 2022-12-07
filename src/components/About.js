47a0bbcece84714c60bc9cc30e09846d828b0ff4
Rishav <rishavkumar99057@gmail.com>
2022-12-07 23:26:05 +0530

    added About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import background from "../components/images/about_bg.png";
+import { Link } from 'react-router-dom'
+import Button from '@mui/material/Button'
 import "./about.css"
 const About = () => {
     return (
@@ -9,8 +11,21 @@ const About = () => {
                 <div className='about__header__right'><p>We have been serving irresistible comfort food since <br/>1999. Our mission is to keep you smiling with every bite.</p>
                 <p>Treat yourself to a feel-good meal today!</p></div>
             </div>
-            <div classname='about__footer' style={{ backgroundImage: `url(${background})`, height: "100vh",backgroundSize: "cover", backgroundRepeat: "no-repeat" }}>
-                
+            <div classname='about__content' style={{ backgroundImage: `url(${background})`, height: "100vh",backgroundSize: "cover", backgroundRepeat: "no-repeat" }}>
+                <div className='about__content__name'>
+                    <h1>THE TSB </h1>
+                    <h6>──────────by─────────</h6>
+                    <h4>Rishav</h4>
+                </div>
+                <div className='about__contents__click'>
+                <li>
+                    <ul><Button variant="text" target='blank'><Link to="/reserve">RESERVE</Link></Button></ul>
+                    <ul><Button variant="text"><Link to="/reserve">MENUS</Link></Button></ul>
+                    <ul><Button variant="text"><Link to="/reserve">EXPERIENCES</Link></Button></ul>
+                    <ul><Button variant="text"><Link to="/reserve">GALLERY</Link></Button></ul>
+                    <ul><Button variant="text"><Link to="/reserve">PRIVATE EVENTS</Link></Button></ul>
+                </li>
+                </div>
             </div>
         </div>
     )
